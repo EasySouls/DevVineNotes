6ad844b4c13e573bd7b2f6a4892feeb65f373ce9
EasySouls <tarjanyicsanad@gmail.com>
2023-12-11 20:07:39 +0100

    Added Firebase Hosting

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ErrorPage from "./components/ErrorPage.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/*' element={<App />} errorElement={<ErrorPage />} />
+        <Route path='/*' element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
